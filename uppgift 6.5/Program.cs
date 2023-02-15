3dd8a8271328de92a8d443a8ca9580cb4f9946e7
Pingvu <nhlpool27@gmail.com>
2023-02-15 11:23:47 +0100

    Add project files.

---
+++ uppgift 6.5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            längd("3","54");
+
+        }
+
+        static void längd(string ord1,string ord2)
+        {
+            if (ord1.Length > ord2.Length)
+            {
+                Console.WriteLine($"{ord1} är längre en {ord2}");
+            }
+
+            else if (ord1.Length < ord2.Length)
+            {
+                Console.WriteLine($"{ord2} är längre än {ord1}");
+            }
+
+            else
+            {
+                Console.WriteLine($"{ord1} och {ord2} är lika långa");
+            }
+        }
+    }
+}
+
